7485b7d7d00322088e2c10e673e1ac96943c6a7e
ruiange <bombingpig@outlook.com>
2024-10-23 11:25:15 +0800

    读取环境变量

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import router from "./router/index.js";
-
+import dotenv from "dotenv";
+// 加载.env文件中的环境变量
+dotenv.config();
 const app = express();
 app.use(router);
 const port = 3001;
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const appid = process.env.APPID || 0
+
     res.send(`我真的太想进步了~！${appid}`);
 });
 
