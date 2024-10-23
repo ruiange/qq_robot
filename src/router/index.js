ab29a3862c047a459dde649425e9e4077ed81d70
ruiange <bombingpig@outlook.com>
2024-10-23 11:17:40 +0800

    看看效果

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('我真的太想进步了~！');
+    const appid = process.env.APPID || 0
+    res.send(`我真的太想进步了~！${appid}`);
 });
 
 export default router;
