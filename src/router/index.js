687f5d4d873c33d7a947f66e89e5b5a678169c4b
ruiange <bombingpig@outlook.com>
2024-10-23 14:58:35 +0800

    我真的很想进步啊~

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,15 @@ import getAppAccessToken from "../utils/getAppAccessToken.js";
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    console.log('访问方式：get')
     const appid = process.env.APPID || 0
-    const accessToken =await getAppAccessToken()
-    res.send(`我真的太想进步了~！${appid} ${accessToken}`);
+    // const accessToken =await getAppAccessToken()
+    // res.send(`我真的太想进步了~！${appid} ${accessToken}`);
+    res.send(`我真的太想进步了~！${appid}`);
+});
+router.post('/', async (req, res) => {
+    console.log('访问方式：post')
+    res.send('test');
 });
 
 export default router;
