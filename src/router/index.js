296e40864e7d42ef5f9fbf79d10ed82968c6171e
ruiange <bombingpig@outlook.com>
2024-10-23 15:22:56 +0800

    事件订阅与通知

--- src/router/index.js
+++ src/router/index.js
@@ -15,46 +15,10 @@ router.post('/', async (req, res) => {
 
     const { body, botSecret } = req;
 
-    // 解析请求体中的 payload
-    let payload;
-    try {
-        payload = JSON.parse(body);
-    } catch (err) {
-        console.error('parse http payload err', err);
-        return;
-    }
-
-    // 解析 payload 中的 data 字段
-    let validationPayload;
-    try {
-        validationPayload = JSON.parse(payload.data);
-    } catch (err) {
-        console.error('parse http payload failed:', err);
-        return;
-    }
-
-    // 生成签名所需的 seed
-    let seed = botSecret;
-    while (seed.length < 32) { // ed25519 的种子大小通常是 32 字节
-        seed += seed;
-    }
-    seed = seed.substring(0, 32);
-
-    // 使用 crypto 模块生成签名
-    const privateKey = crypto.createPrivateKey({ key: seed, format: 'der', type: 'ed25519' });
-    const message = validationPayload.eventTs + validationPayload.plainToken;
-    const signature = crypto.createSign('sha512').update(message).sign(privateKey, 'hex');
-
-    // 构建响应体
-    const response = {
-        plainToken: validationPayload.plainToken,
-        signature: signature,
-    };
-
-    console.error(response)
+    console.warn(body, botSecret)
 
     // 发送响应
-    res.json(response);
+    res.json({code:0});
 });
 
 export default router;
