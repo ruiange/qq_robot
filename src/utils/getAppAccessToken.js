fc841c610cf5096dec2ffd7ca4520003fadc08fc
ruiange <bombingpig@outlook.com>
2024-10-23 14:45:01 +0800

    我真的很想进步啊

--- src/utils/getAppAccessToken.js
+++ src/utils/getAppAccessToken.js
@@ -1,23 +1,28 @@
 import axios from "axios";
 import dayjs from "dayjs";
-import path from "path";
 import fs from "fs";
 
 
-const filePath = path.join(process.cwd(), `src/data/access_token.json`);
-let access_token_data = JSON.parse(fs.readFileSync(filePath).toString())
+// const filePath = path.join(process.cwd(), `src/data/access_token.json`);
+// let access_token_data = JSON.parse(fs.readFileSync(filePath).toString())
 
+let tokenInfo = {
+    accessToken: '',
+    expireTime: '',
+    maturityTime: ''
+}
 const url = 'https://bots.qq.com/app/getAppAccessToken'
 /*获取调用凭证*/
 const getAppAccessToken = async () => {
-    try{
+    try {
 
-        if (access_token_data.accessToken) {
 
-            if (dayjs(access_token_data.maturityTime).isBefore(dayjs())) {
-                return access_token_data.accessToken
-            }
+        // 判断是否过期
+        if (!dayjs(tokenInfo.maturityTime).isBefore(dayjs())) {
+            console.warn('token未过期')
+            return tokenInfo.accessToken
         }
+
         const {data} = await axios({
             url: url,
             method: 'POST',
@@ -32,14 +37,14 @@ const getAppAccessToken = async () => {
         console.warn('当前时间' + dayjs(currentTime).format('YYYY-MM-DD HH:mm:ss'))
         console.warn('过期时间' + dayjs(maturityTime).format('YYYY-MM-DD HH:mm:ss'))
         const accessToken = data.access_token
-        const tokenInfo = {
+        tokenInfo = {
             accessToken,
             expireTime,
             maturityTime: dayjs(maturityTime).format('YYYY-MM-DD HH:mm:ss')
         }
         fs.writeFileSync(filePath, JSON.stringify(tokenInfo))
         return accessToken
-    }catch (e){
+    } catch (e) {
         console.error('获取access_token失败')
         console.error(e.message)
     }
