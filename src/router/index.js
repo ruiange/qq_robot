e4611892c52540b5e05bb848f679a4d4e9ff8348
ruiange <bombingpig@outlook.com>
2024-10-23 14:29:23 +0800

    获取调用凭证

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 // src/router/index.js
 import express from 'express';
+import getAppAccessToken from "../utils/getAppAccessToken.js";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     const appid = process.env.APPID || 0
-
-    res.send(`我真的太想进步了~！${appid}`);
+    const accessToken =await getAppAccessToken()
+    res.send(`我真的太想进步了~！${appid} ${accessToken}`);
 });
 
 export default router;
